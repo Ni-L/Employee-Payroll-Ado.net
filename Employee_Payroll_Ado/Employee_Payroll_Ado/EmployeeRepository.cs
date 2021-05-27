b9a01ae704a7af0e709feaff0f2fca88b57e7e88
“Aniket_D” <aniketdakhare@gmail.com>
2021-05-27 11:18:39 +0530

    [Nilima]Added Connection with db

---
+++ Employee_Payroll_Ado/Employee_Payroll_Ado/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_Ado
+{
+    class EmployeeRepository
+    {
+        ///Specifying the connection string from the sql server connection.
+        public static string connectionString = @"Data Source=DESKTOP-DL043RM;Initial Catalog=Employee_Payroll;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        /// Establishing the connection using the Sql
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        /// <summary>
+        ///UC1 Creating a method for checking for the validity of the connection.
+        /// </summary>
+        public void EnsureDataBaseConnection()
+        {
+            this.connection.Open();
+            using (connection)
+            {
+                Console.WriteLine("The Connection is created");
+            }
+            this.connection.Close();
+        }
+
+    }
+}
---
+++ Employee_Payroll_Ado/Employee_Payroll_Ado/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_Ado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET");
+            //Creating a instance object of EmployeeRepository class.
+            EmployeeRepository repository = new EmployeeRepository();
+            // UC1 Ensuring the database connection using the sql connection string
+            repository.EnsureDataBaseConnection();
+            Console.ReadLine();
+        }
+    }
+}
